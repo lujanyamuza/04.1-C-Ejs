38b13ff8d1c7e85fdfc9d08e18568df7ad5115c1
Jaly <jesuslujany@gmail.com>
2023-05-09 11:13:53 +0200

    ui

---
+++ Methods/EJ1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("Escribe el primer número: ");
+int uno = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Escribe el segundo número: ");
+int dos = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("El número mayor es: " + GetMax(uno, dos));
+
+
+static int GetMax(int uno, int dos)
+{
+    return Math.Max(uno, dos);
+}
