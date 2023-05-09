0d00b48dd44b02da2f8cce828af4daa677c0c96b
Jaly <jesuslujany@gmail.com>
2023-05-09 12:20:01 +0200

    3

---
+++ Methods/EJ3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Write a method that counts how many times given number appears in given array.
+
+Console.WriteLine("Dime el tamaño del array: ");
+int arraysize = int.Parse(Console.ReadLine());
+
+int[] array = new int[arraysize];
+Console.WriteLine("Introduce los números del array: ");
