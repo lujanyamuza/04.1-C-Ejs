70f31d3ee8035177a11d0e67445787edf67b0789
Jaly <jesuslujany@gmail.com>
2023-05-11 22:41:54 +0200

    f

--- Strings/EJ (1)/Program.cs
+++ Strings/EJ (1)/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+
+class ReverseString
+{
+    static void Main()
+    {
+        Console.Write("\nEnter a string: ");
+        string str = Reverse(Console.ReadLine());
+        Console.WriteLine("\nReversed string -> {0}\n", str);
+    }
+
+    static string Reverse(string str)
+    {
+        string tmp = string.Empty;
+
+        for (int i = 0; i < str.Length; i++)
+            tmp = str[i] + tmp;
+
+        return tmp;
+    }
+}
