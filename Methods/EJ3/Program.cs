48c0c2155fc8020c35ba2011196250242491cced
Jaly <jesuslujany@gmail.com>
2023-05-11 22:39:45 +0200

    okay

--- Methods/EJ3/Program.cs
+++ Methods/EJ3/Program.cs
@@ -1,7 +1,29 @@
 ﻿// Write a method that counts how many times given number appears in given array.
 
+static int NumerodeRepeticiones(int[] array, int numbuscado)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] == numbuscado)
+            count ++;
+
+    return count;
+
+}
+
 Console.WriteLine("Dime el tamaño del array: ");
 int arraysize = int.Parse(Console.ReadLine());
 
 int[] array = new int[arraysize];
 Console.WriteLine("Introduce los números del array: ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write("   {0}: ", i + 1);
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+Console.Write("Número buscado: ");
+        int numbuscado = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("El numero {0} aparece {1} veces en el array", numbuscado,
+            NumerodeRepeticiones(array, numbuscado));
